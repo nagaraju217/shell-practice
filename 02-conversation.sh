64ea2cd81dc7c848465b2df87e312ae2091f0b33
nagarajugundala2@gmail.com <nagaraju217@>
2025-09-26 09:34:33 -0400

    variables declaration

---
+++ 02-conversation.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "raju:: How are you ram"
+echo "ram:: i am fine .how about you raj"
+echo "raj :: i am good and thank you ram"
---
+++ 03-variables.sh
@@ -0,0 +1,7 @@
+#!/bin.bash
+PERSON1= $raju
+PERSON2=$ram
+
+echo "$PERSON1:: How are you $PERSON2"
+echo "$PERSON2:: i am fine .how about you $PERSON1"
+echo "$PERSON1 :: i am good and thank you $PERSON2"
