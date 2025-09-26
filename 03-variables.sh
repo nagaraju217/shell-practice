a0c8b96f8058eba964dcde97836f74722b1c2c03
nagarajugundala2@gmail.com <nagaraju217@>
2025-09-26 09:53:21 -0400

    variables updated2

--- 03-variables.sh
+++ 03-variables.sh
@@ -4,5 +4,5 @@ PERSON1=ram
 PERSON2=raj
 
 echo "$PERSON1:: How are you $PERSON2"
-echo "$PERSON1:: i am fine .how about you $PERSON2"
-echo "$PERSON1 :: i am good and thank you $PERSON1"
+echo "$PERSON2:: i am fine .how about you $PERSON1"
+echo "$PERSON1 :: i am good and thank you $PERSON2"
