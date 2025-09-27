3c3aa4967910f9e6e2ccdf3109384cdad3caa544
nagarajugundala2@gmail.com <nagaraju217@>
2025-09-27 07:07:34 -0400

    mysql installation

--- 12-mysql_installation.sh
+++ 12-mysql_installation.sh
@@ -3,14 +3,14 @@ USER_ID=(id -u)
 
 if [ $USER_ID -ne 0 ];then
     echo "please run the script with root user"
-    EXIT 1
+    exit 1
 fi
 
 dnf install mysql -y
 
 if [ $? -ne 0 ]; then
     echo "mysql installing is failure"
-    EXIT 1
+    exit 1
 else
     echo "mysql installation sucess"
 fi
