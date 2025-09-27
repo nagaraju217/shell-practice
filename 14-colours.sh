374a064a5d4b9ba3c177f85798b7156c6ecd1820
nagarajugundala2@gmail.com <nagaraju217@>
2025-09-27 11:21:16 -0400

    colors

---
+++ 14-colours.sh
@@ -0,0 +1,34 @@
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+
+
+if [ $USERID -ne 0 ]; then
+    echo "ERROR:: Please run this script with root privelege"
+    exit 1 # failure is other than 0
+fi
+
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+    echo "$2 installing is $Y failure $N"
+    exit 1
+else
+    echo "$2 installed $Y sucessfulley $N"
+fi
+
+}
+
+
+
+dnf install mysql -y
+VALIDATE $? "mysql"
+
+dnf install nginx -y
+VALIDATE $? "nginx"
+
+dnf install python3 -y
+VALIDATE $? "python3"
