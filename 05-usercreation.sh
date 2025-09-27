990150703f31ea1045fc114fb592d6ad2ed88058
nagarajugundala2@gmail.com <nagaraju217@>
2025-09-27 04:48:33 -0400

    variables updated2

--- 05-usercreation.sh
+++ 05-usercreation.sh
@@ -2,6 +2,9 @@
 echo "enter user name"
 read -s user_name
 sudo useradd $user_name
+echo "user created is : $user_name"
+echo "enter password "
 read -s password
 sudo passwd $user_name
+echo "user created and password set for :$user_name"
 cat /etc/passwd |grep -i $user_name
